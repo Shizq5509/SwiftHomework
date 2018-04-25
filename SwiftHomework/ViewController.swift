2af61b8583df39e4fa24e2b07cb09ea97a3fee1b
q_5509 <shi.zhengqian@immomo.com>
2018-04-25 16:12:02 +0800

    Initial Commit

---
+++ SwiftHomework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftHomework
+//
+//  Created by shi.zhengqian on 2018/4/25.
+//  Copyright © 2018年 shi.zhengqian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
